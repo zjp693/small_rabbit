b24fb5127ca9895e373b683dacff83de935d71f5
zjp693 <942680978@qq.com>
2021-11-19 10:36:44 +0800

    实现面包屑组件和动画

--- src/views/category/SubCategoryPage.vue
+++ src/views/category/SubCategoryPage.vue
@@ -1,16 +1,69 @@
 <template>
   <AppLayout>
-    <div>subcategory</div>
+    <div class="container"></div>
+    <XtxBread>
+      <XtxBreadItem path="/">首页</XtxBreadItem>
+      <XtxBreadItem :path="`/category/${category.topCategory?.id}`">
+        {{ category.topCategory?.name }}
+      </XtxBreadItem>
+      <Transition name="fade-right" mode="out-in">
+        <XtxBreadItem :key="category.subCategory?.id">
+          {{ category.subCategory?.name }}
+        </XtxBreadItem>
+      </Transition>
+    </XtxBread>
   </AppLayout>
 </template>
 
 <script>
 import AppLayout from "@/components/AppLayout";
+import XtxBread from "@/components/library/XtxBread";
+import XtxBreadItem from "@/components/library/XtxBreadItem";
+import { useStore } from "vuex";
+import { useRoute } from "vue-router";
+import { computed } from "vue";
 
 export default {
   name: "SubCategoryPage",
-  components: { AppLayout },
+  components: { AppLayout, XtxBread, XtxBreadItem },
+  setup() {
+    const category = useBread();
+    return { category };
+  },
 };
+function useBread() {
+  //  获取store对象
+  const store = useStore();
+  //  获取route 对象
+  const route = useRoute();
+  //  根据二级分类id 查找二级分类和一级分类
+  return computed(() => {
+    //用于存储一级分类和二级分类的对象
+    const result = {};
+    //  遍历一级分类
+    store.state.category.list.forEach((topCategory) => {
+      //  遍历二级分类
+      topCategory.children?.forEach((subCategory) => {
+        //  查找当前二级分类
+        // console.log(topCategory.id, route.params.id);
+        if (topCategory.id === route.params.id) {
+          //  存储一级分类
+          result.topCategory = {
+            name: topCategory.name,
+            id: topCategory.id,
+          };
+          //  存储二级分类
+          result.subCategory = {
+            name: subCategory.name,
+            id: subCategory.id,
+          };
+        }
+      });
+    });
+    // console.log(result);
+    return result;
+  });
+}
 </script>
 
 <style scoped></style>
--- src/views/category/TopCategoryPage.vue
+++ src/views/category/TopCategoryPage.vue
@@ -3,9 +3,11 @@
     <div class="container">
       <XtxBread>
         <XtxBreadItem path="/"> 首页</XtxBreadItem>
-        <XtxBreadItem :key="`/category/${topCategory?.id}`">
-          {{ topCategory?.name }}</XtxBreadItem
-        >
+        <Transition name="fade-right" mode="out-in">
+          <XtxBreadItem :key="`/category/${topCategory?.id}`">
+            {{ topCategory?.name }}</XtxBreadItem
+          >
+        </Transition>
         <XtxBreadItem :carousels="banners"></XtxBreadItem>
       </XtxBread>
     </div>
@@ -29,24 +31,20 @@ export default {
     const { banners, getData } = useBanners();
     getData(2);
     setTimeout(() => {
-      console.log(topCategory);
+      // console.log(topCategory);
     }, 2000);
     return { topCategory, banners };
   },
 };
 function useCategory() {
-  //  获取路由对象
-  const route = useRoute();
-  //  获取store对象
+  // 获取 store 对象
   const store = useStore();
-  //  获取当前访问的分类信息
-  //  为什么使用计算机属性
-  //  因为刚开始category.list 里面只有name 没有id,
-  //  只有只是数据请求过来的以后才有id,所以需要通过计算机监控数据变化
-  //  当数据发生变化后重新查找分类数据
+  // 获取 route 对象
+  const route = useRoute();
   return computed(() => {
+    // 根据id查找一级分类数据
+    // id 从路由参数中获取
     return store.state.category.list.find(
-      // console.log(store.state.category)
       (item) => item.id === route.params.id
     );
   });
